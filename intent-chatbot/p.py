2d47a779cdb6d5b8777cd141e15bfa1f33eb9da8
prateek-prog <highestplayerjobs@gmail.com>
2025-02-25 18:54:32 +0530

    Update p.py

--- intent-chatbot/p.py
+++ intent-chatbot/p.py
@@ -29,10 +29,10 @@ def chatbot  (input_text, greenskills):
     if entity_response and year_response:
      for country_data in greenskills:
         if entity_response and year_response:
-            if country_data["Entity"].casefold() == entity_response.lower() and country_data["Year"] == year_response:
+            if country_data["Entity"].casefold() == entity_response.casefold() and country_data["Year"] == year_response:
                 filtered_data.append(country_data)
         elif entity_response:
-            if country_data["Entity"].casefold() == entity_response.lower():
+            if country_data["Entity"].casefold() == entity_response.casefold():
                 filtered_data.append(country_data)
         elif year_response:
             if country_data["Year"] == year_response:
@@ -78,31 +78,31 @@ def chatbot  (input_text, greenskills):
                  responses.append(f"{country_data['Entity']} ({country_data['Year']}): Electricity from fossil fuels (TWh) is {fossil_fuel_electricity}TWh.")
             elif "financial flow for developing countries" in input_text:
                 financial_developement = country_data.get("Financial flows to developing countries (US $)","Data not avaliable")  
-                responses.append(f"{country_data['Entity']} ({country_data['Year']}):Financial flows to developing countries (US $) is {financial_developement}TWh.")
+                responses.append(f"{country_data['Entity']} ({country_data['Year']}): Financial flows to developing countries (US $) is {financial_developement}TWh.")
             elif "low carbon electricity" in input_text:
                 low_carbon_electricity = country_data.get("Low-carbon electricity (% of total electricity)","Data not avaliable")
-                responses.append(f"{country_data['Entity']} ({country_data['Year']}):Low-carbon electricity is {low_carbon_electricity}%.") 
+                responses.append(f"{country_data['Entity']} ({country_data['Year']}): Low-carbon electricity is {low_carbon_electricity}%.") 
             elif "land area" in input_text:
                land_area = country_data.get("Land Area(Km2)","Data not avaliable")
-               responses.append(f"{country_data['Entity']} ({country_data['Year']}):Land Area is {land_area} KM2.")       
+               responses.append(f"{country_data['Entity']} ({country_data['Year']}): Land Area is {land_area} KM2.")       
             elif "density area" in input_text:
                 density_area= country_data.get("Density\\n(P/Km2)","Data not avaliable")
-                responses.append(f"{country_data['Entity']} ({country_data['Year']}):Density of Area is {density_area} P/KM2.")
+                responses.append(f"{country_data['Entity']} ({country_data['Year']}): Density of Area is {density_area} P/KM2.")
             elif "Primary energy consumption per capita" in input_text:
                 energy_consumption= country_data.get("Primary energy consumption per capita (kWh/person)","Data not avaliable")
-                responses.append(f"{country_data['Entity']} ({country_data['Year']}):Density of Area is {energy_consumption} KWh/person.")
+                responses.append(f"{country_data['Entity']} ({country_data['Year']}): Density of Area is {energy_consumption} KWh/person.")
             elif "energy intensity level of primary energy" in input_text:
                 energy_intensity_level=country_data.get("Energy intensity level of primary energy (MJ/$2017 PPP GDP)","Data not avaliable")
                 responses.append(f"{country_data['Entity']} ({country_data['Year']}): Energy intensity level of primary energy is {energy_intensity_level} MJ/$2017 PPP GDP.")
             elif "renewables" in input_text:
                 renewable_energy_share1= country_data.get("Renewables (% of equivalent primary energy)","Data not avaliable")
-                responses.append(f"{country_data['Entity']} ({country_data['Year']})Renewables of equivalent primary energy is {renewable_energy_share1} %.")
+                responses.append(f"{country_data['Entity']} ({country_data['Year']}): Renewables of equivalent primary energy is {renewable_energy_share1} %.")
             elif "renewable electricity generating capacity per capita" in input_text:
                 renewable_electricity= country_data.get("Renewable-electricity-generating-capacity-per-capita","Data not avaliable")
-                responses.append(f"{country_data['Entity']} ({country_data['Year']})Renewables of equivalent primary energy is {renewable_electricity} W/person.")
+                responses.append(f"{country_data['Entity']} ({country_data['Year']}): Renewables of equivalent primary energy is {renewable_electricity} W/person.")
             elif "renewable electricity" in input_text:
                 renewable_electricity_use= country_data.get("Electricity from renewables (TWh)","Data not avaliable")
-                responses.append(f"{country_data['Entity']} ({country_data['Year']}):Renewable electricity is {renewable_electricity_use} TWh.")
+                responses.append(f"{country_data['Entity']} ({country_data['Year']}): Renewable electricity is {renewable_electricity_use} TWh.")
         # Return all filtered responses
        # return entity_response + "\n" + "\n".join(responses)
         return "\n"+ "\n".join(responses)
